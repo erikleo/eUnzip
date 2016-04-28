d1d238ad800208d57a0fc1cd62392e78a6bd4583
Erik Leo <eleo7@bloomberg.net>
2016-04-28 12:44:52 -0400

    Update install bat

--- CopyToStendTo.bat
+++ CopyToStendTo.bat
@@ -1,2 +1,7 @@
+rem older reg modification
+rem REG ADD "HKCU\Software\Microsoft\Command Processor" /V DisableUNCCheck /T REG_DWORD /F /D 1
+
+
+rem do copy
 copy .\eunzip\bin\Release\eUnzip.exe %APPDATA%\Microsoft\Windows\SendTo\
 Pause
